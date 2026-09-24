f6ca1f3f6b086b34ea8da3aea483264b5535196b
Developer <11658@example.com>
2026-09-24 14:23:19 +0800

    align outbound bodies and violation errors with rc40

--- relay/common/outbound_body.go
+++ relay/common/outbound_body.go
@@ -22,10 +22,10 @@ import (
 // transport from prematurely closing the underlying BodyStorage. The returned
 // size is meant to be propagated to http.Request.ContentLength because the
 // type-erased io.Reader prevents net/http from auto-detecting it.
-func NewOutboundJSONBody(data []byte) (body io.Reader, size int64, closer io.Closer, err error) {
+func NewOutboundJSONBody(data []byte) (body io.Reader, closer io.Closer, err error) {
 	storage, err := common.CreateBodyStorage(data)
 	if err != nil {
-		return nil, 0, nil, err
+		return nil, nil, err
 	}
-	return struct{ io.Reader }{Reader: storage}, storage.Size(), storage, nil
+	return storage, storage, nil
 }
